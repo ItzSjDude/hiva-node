0f37d951746274fdb308f7631c2cc1a5ac6ccbfe
ItzSjDude <Support@itzsjdude.in>
2025-08-14 15:17:19 +0530

    FIX: Audio Party Seats Model

--- src/models/AudioPartySeats.js
+++ src/models/AudioPartySeats.js
@@ -81,75 +81,75 @@ module.exports = AudioPartySeat;
 
 
 
-// src/models/AudioParty.js
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+// // src/models/AudioParty.js
+// const { DataTypes } = require('sequelize');
+// const sequelize = require('../config/database');
 
-const AudioParty = sequelize.define('AudioParty', {
-  id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    primaryKey: true,
-  },
-  title: {
-    type: DataTypes.STRING(150),
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.TEXT,
-    allowNull: true,
-  },
-  hostId: {
-    type: DataTypes.BIGINT.UNSIGNED,
-    allowNull: false,
-  },
-  livekitRoomName: {
-    type: DataTypes.STRING(100),
-    allowNull: false,
-    unique: true,
-  },
-  maxParticipants: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    defaultValue: 50,
-  },
-  isActive: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false,
-    defaultValue: true,
-  },
-  isPrivate: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false,
-    defaultValue: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  startTime: {
-    type: DataTypes.DATE,
-    allowNull: true,
-  },
-  endTime: {
-    type: DataTypes.DATE,
-    allowNull: true,
-  },
-  tags: {
-    type: DataTypes.JSON,
-    allowNull: true,
-  },
-  participantCount: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    defaultValue: 0,
-  }
-}, {
-  tableName: 'audio_parties',
-  timestamps: true,
-  underscored: true,
-});
+// const AudioParty = sequelize.define('AudioParty', {
+//   id: {
+//     type: DataTypes.UUID,
+//     defaultValue: DataTypes.UUIDV4,
+//     primaryKey: true,
+//   },
+//   title: {
+//     type: DataTypes.STRING(150),
+//     allowNull: false,
+//   },
+//   description: {
+//     type: DataTypes.TEXT,
+//     allowNull: true,
+//   },
+//   hostId: {
+//     type: DataTypes.BIGINT.UNSIGNED,
+//     allowNull: false,
+//   },
+//   livekitRoomName: {
+//     type: DataTypes.STRING(100),
+//     allowNull: false,
+//     unique: true,
+//   },
+//   maxParticipants: {
+//     type: DataTypes.INTEGER,
+//     allowNull: false,
+//     defaultValue: 50,
+//   },
+//   isActive: {
+//     type: DataTypes.BOOLEAN,
+//     allowNull: false,
+//     defaultValue: true,
+//   },
+//   isPrivate: {
+//     type: DataTypes.BOOLEAN,
+//     allowNull: false,
+//     defaultValue: false,
+//   },
+//   password: {
+//     type: DataTypes.STRING,
+//     allowNull: true,
+//   },
+//   startTime: {
+//     type: DataTypes.DATE,
+//     allowNull: true,
+//   },
+//   endTime: {
+//     type: DataTypes.DATE,
+//     allowNull: true,
+//   },
+//   tags: {
+//     type: DataTypes.JSON,
+//     allowNull: true,
+//   },
+//   participantCount: {
+//     type: DataTypes.INTEGER,
+//     allowNull: false,
+//     defaultValue: 0,
+//   }
+// }, {
+//   tableName: 'audio_parties',
+//   timestamps: true,
+//   underscored: true,
+// });
 
-module.exports = AudioParty;
+// module.exports = AudioParty;
 
 
